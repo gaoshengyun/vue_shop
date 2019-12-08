301610a52e9861783368575c04d94984309cf943
gaoshengyun <18616242909@163.com>
2019-12-08 14:49:15 +0800

    main-page-header

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,7 +1,21 @@
 <template>
-  <div>
-    <el-button @click="logout" type="info">退出</el-button>
-  </div>
+  <el-container class="home-container">
+    <!-- 头部区域 -->
+    <el-header>
+      <div>
+        <img src="../assets/heima.png" alt="">
+        <span>电商后台管理系统</span>
+      </div>
+      <el-button @click="logout" type="info">退出</el-button>
+    </el-header>
+    <!-- 页面主体区 -->
+    <el-container>
+      <!-- 侧边栏 -->
+      <el-aside width="200px">aside</el-aside>
+      <!-- 右侧内容主体 -->
+      <el-main>Main</el-main>
+    </el-container>
+  </el-container>
 </template>
 <script>
 export default {
@@ -15,4 +29,29 @@ export default {
 }
 </script>
 <style lang="scss" scoped>
+.home-container {
+  height: 100%;
+}
+.el-header {
+  background-color: #373d41;
+  display: flex;
+  justify-content: space-between;
+  padding-left: 0;
+  align-items: center;
+  div{
+    display: flex;
+    align-items: center;
+    span {
+      color: #fff;
+      font-size: 20px;
+      margin-left: 15px;
+    }
+  }
+}
+.el-aside {
+  background-color: #333744;
+}
+.el-main {
+  background-color: #eaedf1;
+}
 </style>
