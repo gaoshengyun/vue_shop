7522fbd0289831b0b632eb7c4c6b98c5d42b746c
gaoshengyun <18616242909@163.com>
2019-12-13 16:12:39 +0800

    全局progressbar完成

--- src/main.js
+++ src/main.js
@@ -11,11 +11,21 @@ import VueQuillEditor from 'vue-quill-editor'
 import 'quill/dist/quill.core.css'
 import 'quill/dist/quill.snow.css'
 import 'quill/dist/quill.bubble.css'
+// 导入nprogress
+import NProgress from 'nprogress'
+import 'nprogress/nprogress.css'
 // 配置请求的根路径
 axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
 // 设置拦截器
+// 在request中展示进度条
 axios.interceptors.request.use(config => {
+  NProgress.start()
   config.headers.Authorization = window.sessionStorage.getItem('token')
+  return config
+})
+// 在response 拦截器中隐藏进度条
+axios.interceptors.response.use(config => {
+  NProgress.done()
   return config
 })
 Vue.prototype.$http = axios
